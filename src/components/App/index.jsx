f68b616aa3dfefa44c909c4368e0e6a2f27f4538
jeffersoninacio@hotmail.com <jeffersoninacio@hotmail.com>
2019-08-15 19:24:21 -0300

    Primeiro commit
    Montada a estrutura do projeto.

---
+++ src/components/App/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <h1>Navbar</h1>
+      </header>
+      <main>
+        <h1>Home</h1>
+      </main>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </>
+  );
+}
+
+export default App;
